b686327d7ae865c3e41fd68eb1fdd64a9d375711
David Geiss <dave.geiss@gmail.com>
2017-06-15 13:00:47 -0400

    Finished workshop

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ module.exports = function makeRouterWithSockets(io) {
 
   // a reusable function
   function respondWithAllTweets(req, res, next) {
-    client.query('SELECT * FROM tweets', function (err, result) {
+    client.query('SELECT * FROM tweets JOIN users ON users.id = tweets.user_id', function (err, result) {
       if (err) return next(err); // pass errors to Express
       var tweets = result.rows;
       res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -35,24 +35,33 @@ module.exports = function makeRouterWithSockets(io) {
 
   // single-tweet page
   router.get('/tweets/:id', function (req, res, next) {
-    
-    res.render('index', {
-      title: 'Twitter.js',
-      tweets: tweetsWithThatId // an array of only one element ;-)
+    client.query('SELECT * FROM users JOIN tweets ON users.id = tweets.user_id AND tweets.id = $1', [req.params.id], (err, results) => {
+      if (err) return next(err);
+      let tweetsForID = results.rows;
+      res.render('index', {
+        title: 'Twitter.js',
+        tweets: tweetsForID // an array of only one element ;-)
+      });
     });
   });
 
   // // create a new tweet
-  // router.post('/tweets', function (req, res, next) {
-  //   var newTweet = tweetBank.add(req.body.name, req.body.content);
-  //   io.sockets.emit('new_tweet', newTweet);
-  //   res.redirect('/');
-  // });
+  router.post('/tweets', function (req, res, next) {
+    client.query('SELECT id FROM users WHERE users.name = $1', [req.body.name], (err, Uresults) => {
+      if (err) return next(err);
+      client.query('INSERT INTO tweets (user_id, content) VALUES($1, $2) RETURNING *', [Uresults.rows[0].id, req.body.content], (err, results) => {
+        if (err) return next(err);
+        console.log(Uresults.rows[0].picture_url);
+        io.sockets.emit('new_tweet', {name: req.body.name, content: req.body.content, picture_url: Uresults.rows[0].picture_url, id: results.rows[0].id});
+        res.redirect('/');
+      });
+    });
+  });
 
-  // // replaced this hard-coded route with general static routing in app.js
-  // router.get('/stylesheets/style.css', function(req, res, next){
-  //   res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
-  // });
+  // replaced this hard-coded route with general static routing in app.js
+  router.get('/stylesheets/style.css', function (req, res, next) {
+    res.sendFile('/stylesheets/style.css', { root: __dirname + '/../public/' });
+  });
 
   return router;
 };
